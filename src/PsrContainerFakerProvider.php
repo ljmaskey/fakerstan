ed8e6e0fedabe5bd848a641b1440ed5ae0885031
Lincoln Maskey <1082557+ljmaskey@users.noreply.github.com>
2024-12-31 10:06:52 -0400

    Remove unnecessary comments.

--- src/PsrContainerFakerProvider.php
+++ src/PsrContainerFakerProvider.php
@@ -34,15 +34,12 @@ final class PsrContainerFakerProvider implements FakerProvider
             throw new RuntimeException('Could not read container PHP file');
         }
 
-        // Include the container file and store the return value, just in case.
         $maybeContainer = require_once $this->phpContainerPath;
 
-        // If the code in that file returns nothing, the require_once will return 1.
         if (is_null($this->setsVariable) && ($maybeContainer === 1)) {
             throw new RuntimeException('Container file was expected to return the container, but it returned nothing');
         }
 
-        // If we didn't expect it to be returned, check the variable that we said it would be in.
         if (is_string($this->setsVariable)) {
             $definedVariables = get_defined_vars();
             if (! array_key_exists($this->setsVariable, $definedVariables)) {
@@ -52,17 +49,14 @@ final class PsrContainerFakerProvider implements FakerProvider
             $maybeContainer = $definedVariables[$this->setsVariable];
         }
 
-        // Check that we got something that looks like a container.
         if (! $maybeContainer instanceof ContainerInterface) {
             throw new RuntimeException('Retrieved container is not a '.ContainerInterface::class);
         }
 
-        // Does the container have something with the expected Faker Generator ID?
         if (! $maybeContainer->has($this->containerFakerId)) {
             throw new RuntimeException('Container does not have entry with ID '.$this->containerFakerId);
         }
 
-        // Make sure that we retrieved an actual Generator.
         $containerFaker = $maybeContainer->get($this->containerFakerId);
         if (! $containerFaker instanceof Generator) {
             throw new RuntimeException('Container entry with ID '.$this->containerFakerId.' is not a '.Generator::class);
