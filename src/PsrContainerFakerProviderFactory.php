e60720291ae57860cf5b9cf38dffc3f11ec81057
Lincoln Maskey <1082557+ljmaskey@users.noreply.github.com>
2024-12-31 13:18:42 -0400

    Allow the container path to be null until we absolutely decide that we want to create a PsrContainerFakerProvider.

--- src/PsrContainerFakerProviderFactory.php
+++ src/PsrContainerFakerProviderFactory.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace CalebDW\Fakerstan;
 
 use Faker\Generator;
+use RuntimeException;
 
 class PsrContainerFakerProviderFactory
 {
     public function __construct(
-        private string $phpContainerPath,
+        private ?string $phpContainerPath = null,
         private ?string $setsVariable = null,
         private string $containerFakerId = Generator::class,
     ) {
@@ -17,6 +18,10 @@ class PsrContainerFakerProviderFactory
 
     public function create(): FakerProvider
     {
+        if (is_null($this->phpContainerPath)) {
+            throw new RuntimeException('PsrContainerFakerProviderFactory requires a value for "parameters › fakerstan › psrProvider › phpContainerPath"');
+        }
+
         return new PsrContainerFakerProvider(
             $this->phpContainerPath,
             $this->setsVariable,
